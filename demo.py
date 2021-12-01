66a63994d9017457c657d57d371ce547ba1e1f4d
Émile Jetzer <emile.jetzer@polymtl.ca>
2021-12-01 09:40:27 -0500

    Démo avec octets (résultat bancal)

--- demo.py
+++ demo.py
@@ -36,10 +36,32 @@ cadre = tk.Frame(racine)
 var_écrire: tk.StringVar = tk.StringVar(racine, 'rien')
 var_lecture: tk.StringVar = tk.StringVar(racine, '')
 
+
+dictionnaire = {'a': 0b00001,
+                'l': 0b00010,
+                'o': 0b00011,
+                'r': 0b00100,
+                'i': 0b00101,
+                'e': 0b00110,
+                'n': 0b00111}
+
+def f(s:str) -> bytes:
+    return bytes([dictionnaire[c] for c in s])
+
+def finv(b:bytes) -> str:
+    s = ''
+    #for bit in b[:-1]:
+    #    idx = list(dictionnaire.values()).index(bit)
+    #    s = s + dictionnaire.keys()[idx]
+    for a in b:
+        s = s + ' ' + str(hex(a))
+    return s
+
+
 def lire(var=var_lecture, com=com, racine=racine):
     val = var.get()
     try:
-        nouv = str(com.read_until('\n'), encoding='utf-8').strip()
+        nouv = finv(com.read(4))
         if nouv:
             val = nouv
     except UnicodeDecodeError:
@@ -49,9 +71,10 @@ def lire(var=var_lecture, com=com, racine=racine):
 
 def écrire(var=var_écrire, com=com):
     val = var.get()
-    com.write(bytes(val, encoding='utf-8'))
+    print('Valeur brute: ' + val)
+    print('Valeur transformée: ' + repr(f(val)))
+    com.write(f(val))
 
-var_écrire.trace('w', écrire)
 entrée = ttk.Entry(cadre, textvariable=var_écrire, font=('Arial', 36))
 bouton1 = ttk.Button(cadre, text='Envoyer', command=écrire)
 cadran = ttk.Label(cadre, textvariable=var_lecture, font=('Arial', 36))
--- demo/demo.ino
+++ demo/demo.ino
@@ -1,4 +1,4 @@
-String valeur = "rien";
+byte valeur[4] = {4, 5, 6, 7};
 bool envoye = false;
 
 void setup() {
@@ -7,10 +7,16 @@ void setup() {
 
 void loop() {
   if (Serial.available()) {
-    valeur = Serial.readString();
+    //valeur = Serial.readString();
+    for (int idx=0; idx<4; idx++) {
+      byte c = Serial.read() & 0b11111;
+      valeur[idx] = c;
+    }
     envoye = false;
   } else if (!envoye) {
-    Serial.println(valeur);
+    for (int idx=0; idx<4; idx++) {
+      Serial.write(valeur[idx] & 0b11111);
+    }
     envoye = true;
   }
 }
